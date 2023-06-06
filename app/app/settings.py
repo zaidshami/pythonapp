409be94fba501d60223d3b9b9b210d52347d58ed
SCC <zaidalshami8@gmail.com>
2023-06-06 20:19:41 +0300

    user model

--- app/app/settings.py
+++ app/app/settings.py
@@ -38,7 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-     'core',
+        'rest_framework.authtoken',
+
+    'core',
+    'rest_framework',
+    'drf_spectacular',
+    'user'
 ]
 
 MIDDLEWARE = [
@@ -131,3 +136,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 AUTH_USER_MODEL = 'core.User'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -3,11 +3,8 @@ Tests for models.
 """
 from django.test import TestCase
 from django.contrib.auth import get_user_model
-
-
 class ModelTests(TestCase):
     """Test models."""
-
     def test_create_user_with_email_successful(self):
         """Test creating a user with an email is successful."""
         email = 'test@example.com'
@@ -16,11 +13,8 @@ class ModelTests(TestCase):
             email=email,
             password=password,
         )
-
         self.assertEqual(user.email, email)
         self.assertTrue(user.check_password(password))
-
-        
     def test_new_user_email_normalized(self):
         """Test email is normalized for new users."""
         sample_emails = [
@@ -32,3 +26,17 @@ class ModelTests(TestCase):
         for email, expected in sample_emails:
             user = get_user_model().objects.create_user(email, 'sample123')
             self.assertEqual(user.email, expected)
+    def test_new_user_without_email_raises_error(self):
+        """Test that creating a user without an email raises a ValueError."""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user('', 'test123')
+
+    def test_create_superuser(self):
+        """Test creating a superuser."""
+        user = get_user_model().objects.create_superuser(
+            'test@example.com',
+            'test123',
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
